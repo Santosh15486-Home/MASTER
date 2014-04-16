20308e67b1b2867cb98a9627c4aef59bd43fc521
Santosh15486 <santoshphatangare@gmail.com>
2014-04-16 15:42:20 +0530

    After dogra

--- GITTest/src/com/gittest/GitTest.java
+++ GITTest/src/com/gittest/GitTest.java
@@ -5,8 +5,10 @@ public class GitTest {
 		System.out.println("Git is cool");
 		System.out.println("Amit Tiwari");
 		System.out.println("Rajendra Khaire...!");
-		System.out.println("India !!! Please vote");
+		System.out.println("India!!! Please vote");
 		System.out.println("Santosh");
+		System.out.println("Chetan Dong kabhi wrong nahi hota.. ha ha ha ha ha..");
+		System.out.println("Ab ki baar Modi Sarkar!!!");
 		//new Test1().say();
 	}
 }
