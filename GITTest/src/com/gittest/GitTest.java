8cfae9e829d2fbf8a1f6a0797a83467fdbad6710
rajendra <rajendra@GL-temp>
2014-04-16 17:49:04 +0530

    asd

--- GITTest/src/com/gittest/GitTest.java
+++ GITTest/src/com/gittest/GitTest.java
@@ -2,6 +2,12 @@ package com.gittest;
 
 public class GitTest {
 	public static void main(String a[]){
-		System.out.println("Git is cool");
+		System.out.println("Santosh...");
+		System.out.println("CM...!");
+		System.out.println("Amit...!");
+		System.out.println("No Conflicts....");
+		
+		System.out.println("Chetan...........!");
+		System.out.println("Chetan dogra");
 	}
 }
