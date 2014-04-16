97026bbf1da8e2809a720d4f7bc88ad3be753c40
amit <amit@prodonus-neha-003>
2014-04-16 13:21:08 +0530

    j

--- GITTest/src/com/gittest/GitTest.java
+++ GITTest/src/com/gittest/GitTest.java
@@ -3,6 +3,7 @@ package com.gittest;
 public class GitTest {
 	public static void main(String a[]){
 		System.out.println("Git is cool");
+		System.out.println("Amit Tiwari");
 		//new Test1().say();
 	}
 }
