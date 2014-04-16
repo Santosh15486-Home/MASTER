5f7530f1d0fc8896f8c3124e8f745f2850bb7cee
amit <amit@prodonus-neha-003>
2014-04-16 15:31:51 +0530

    india

--- GITTest/src/com/gittest/GitTest.java
+++ GITTest/src/com/gittest/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 		System.out.println("Git is cool");
 		System.out.println("Amit Tiwari");
 		System.out.println("Rajendra Khaire...!");
+		System.out.println("India !!! Please vote");
 		//new Test1().say();
 	}
 }
