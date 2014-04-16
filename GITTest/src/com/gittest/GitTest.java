6f5b493ddd77d1e06891c747512add2f32e7ac6e
amit <amit@prodonus-neha-003>
2014-04-16 15:22:59 +0530

    Raj..!

--- GITTest/src/com/gittest/GitTest.java
+++ GITTest/src/com/gittest/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 	public static void main(String a[]){
 		System.out.println("Git is cool");
 		System.out.println("Amit Tiwari");
+		System.out.println("Rajendra Khaire...!");
 		//new Test1().say();
 	}
 }
