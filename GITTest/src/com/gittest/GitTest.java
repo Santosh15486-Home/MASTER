1345d9f0a77510a433ca4b7042692defa89ae7fc
Santosh15486 <santoshphatangare@gmail.com>
2014-04-16 16:55:50 +0530

    san...

--- GITTest/src/com/gittest/GitTest.java
+++ GITTest/src/com/gittest/GitTest.java
@@ -3,5 +3,7 @@ package com.gittest;
 public class GitTest {
 	public static void main(String a[]){
 		System.out.println("Git is cool");
+		System.out.println("Santosh...");
+		System.out.println("CM...!");
 	}
 }
