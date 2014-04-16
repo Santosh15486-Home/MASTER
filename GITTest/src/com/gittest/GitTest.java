900bcb25283850fc4c078071e3d22f08c52782a5
Santosh15486 <santoshphatangare@gmail.com>
2014-04-16 16:44:54 +0530

    a

--- GITTest/src/com/gittest/GitTest.java
+++ GITTest/src/com/gittest/GitTest.java
@@ -3,16 +3,5 @@ package com.gittest;
 public class GitTest {
 	public static void main(String a[]){
 		System.out.println("Git is cool");
-		System.out.println("Amit Tiwari");
-		System.out.println("Rajendra Khaire...!");
-		System.out.println("India!!! Please vote");
-		System.out.println("Santosh");
-		System.out.println("Chetan Dong kabhi wrong nahi hota.. ha ha ha ha ha..");
-		System.out.println("Ab ki baar Modi Sarkar!!!");
-		System.out.println("Praful kuthjay...?");
-		System.out.println("Ala ka to gharun");
-		System.out.println("hi");
-		System.out.println("Ghari gelay.. ");
-		//new Test1().say();
 	}
 }
