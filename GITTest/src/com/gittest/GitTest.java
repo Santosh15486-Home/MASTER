44c6ad67413205a55b750848ddb3713ccafc5061
rajendra <rajendra@GL-temp>
2014-04-16 17:01:24 +0530

    Commit...!

--- GITTest/src/com/gittest/GitTest.java
+++ GITTest/src/com/gittest/GitTest.java
@@ -2,9 +2,9 @@ package com.gittest;
 
 public class GitTest {
 	public static void main(String a[]){
-		System.out.println("Git is cool");
 		System.out.println("Santosh...");
 		System.out.println("CM...!");
 		System.out.println("Amit...!");
+		System.out.println("No Conflicts....");
 	}
 }
