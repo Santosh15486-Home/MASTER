2b76d94d2ac6cd3047e2fff726547163b9090dda
Santosh15486 <santoshphatangare@gmail.com>
2014-04-16 17:46:38 +0530

    reset

--- GITTest/src/com/gittest/GitTest.java
+++ GITTest/src/com/gittest/GitTest.java
@@ -3,30 +3,5 @@ package com.gittest;
 public class GitTest {
 	public static void main(String a[]){
 		System.out.println("Git is cool");
-		System.out.println("Amit Tiwari");
-		System.out.println("Rajendra Khaire...!");
-		System.out.println("India!!! Please vote");
-		System.out.println("Santosh");
-		System.out.println("Chetan Dong kabhi wrong nahi hota.. ha ha ha ha ha..");
-		System.out.println("Ab ki baar Modi Sarkar!!!");
-		System.out.println("Praful kuthjay...?");
-		System.out.println("Ala ka to gharun");
-		System.out.println("hi");
-		System.out.println("Ghari gelay.. ");
-		System.out.println("12345678+9");
-		//new Test1().say();
-		System.out.println("Santosh...");
-		System.out.println("CM...!");
-		System.out.println("Amit...!");
-		System.out.println("No Conflicts....");
-		System.out.println("Ghe..");
-		System.out.println("want conflictssss...!");
-		
-		System.out.println("Chetan...........!");
-		System.out.println("Chetan dogra");
-		
-		System.out.println("Amit Chakravarti....!");
-		
-		System.out.println(" Last line");
 	}
 }
