26b65719392d238c19c2c9364ef38848a2032942
Santosh15486 <santoshphatangare@gmail.com>
2014-04-16 13:09:02 +0530

    removed new class

--- GITTest/src/com/gittest/GitTest.java
+++ GITTest/src/com/gittest/GitTest.java
@@ -3,6 +3,6 @@ package com.gittest;
 public class GitTest {
 	public static void main(String a[]){
 		System.out.println("Git is cool");
-		new Test1().say();
+		//new Test1().say();
 	}
 }
--- GITTest/src/com/gittest/Test1.java
+++
@@ -1,7 +0,0 @@
-package com.gittest;
-
-public class Test1 {
-	public void say(){
-		System.out.println("Form other class");
-	}
-}
