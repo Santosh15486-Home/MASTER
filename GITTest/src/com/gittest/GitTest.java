5a14bb5ba9579ca030b9a6342ab5792ed74c6230
Santosh15486 <santoshphatangare@gmail.com>
2014-11-05 15:54:23 +0530

    Again...

--- GITTest/src/com/gittest/GitTest.java
+++ GITTest/src/com/gittest/GitTest.java
@@ -3,16 +3,16 @@ package com.gittest;
 public class GitTest {
 	public static void main(String a[]){
 
-		System.out.println("Git is cool");
-		System.out.println("Santosh phatangare");
-
-		System.out.println("Santosh...");
-		System.out.println("CM...!");
-		System.out.println("Amit...!");
-		System.out.println("No Conflicts....");
-		
-		System.out.println("Chetan...........!");
-		System.out.println("Chetan dogra");
+		System.out.println("Git is cool Again.....");
+//		System.out.println("Santosh phatangare");
+//
+//		System.out.println("Santosh...");
+//		System.out.println("CM...!");
+//		System.out.println("Amit...!");
+//		System.out.println("No Conflicts....");
+//
+//		System.out.println("Chetan...........!");
+//		System.out.println("Chetan dogra");
 
 	}
 }
